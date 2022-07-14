9ffcecd9129ac375f4af0f45e2d7f4da4c9880b1
Andrew Marshall <andrewleonmarshall@gmail.com>
2022-07-14 14:03:36 -0400

    Added Angular Materials library

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -8,13 +8,14 @@ import { CareerComponent } from './career/career.component';
 import { CareerEntryComponent } from './career/career-entry/career-entry.component';
 import { CareerListComponent } from './career/career-list/career-list.component';
 import { CareerDetailsComponent } from './career/career-details/career-details.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent, HeaderComponent, BioComponent, CareerComponent, CareerEntryComponent, CareerListComponent, CareerDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
